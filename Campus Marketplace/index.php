4d093bb4a34a3875a3f3f3f89db8c885ab32c5b4
Leeon <leeonsethanoun@gmail.com>
2023-12-19 06:28:33 -0800

    fixed

--- Campus Marketplace/index.php
+++ Campus Marketplace/index.php
@@ -10,7 +10,7 @@
         <h1> Welcome to our Big Hero 6 and Campus Marketplace website</h1>
         <p>Team Big Hero 6's product, envisioned as a dynamic platform for college communities, aims to revolutionize the way students interact with the concept of a campus marketplace.
             Rooted in the idea of sustainability and convenience, this project proposes a unique solution for buying and selling secondhand items within a college environment. We creatively brought to life through a Figma prototype,
-            demonstrates a keen understanding of user needs and preferences. This project is not just about a marketplace; it's about fostering a sense of comunity and environmental responsiblity in college campuses,
+            demonstrates a keen understanding of user needs and preferences. This project is not just about a marketplace; it's about fostering a sense of community and environmental responsiblity in college campuses,
             potentially transforming the way students engage with each other and their environment in marketplace.
         </p>
     </div>
--- Campus Marketplace/prototype.php
+++ Campus Marketplace/prototype.php
@@ -6,6 +6,7 @@
     include($path."asset/inc/nav.php");
 ?>
     <div class="container">
+        <h1>(Click the link to be directed to Figma below each task)</h1>
         <h1><a href="https://www.figma.com/proto/iLIeAbRBS66BlRL32eyGR7/ISTE260-Prototype?page-id=0%3A1&type=design&node-id=50-270&viewport=-860%2C494%2C0.47&t=kdSxI4HEWqdEWbrH-9&scaling=scale-down&starting-point-node-id=50%3A270&show-proto-sidebar=1">Link to old Task 1</a></h1>
     <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="600" height="800"
      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FiLIeAbRBS66BlRL32eyGR7%2FISTE260-Prototype%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D50-274%26viewport%3D-860%252C494%252C0.47%26t%3DIPcRmNBjiYoBcdG7-1%26scaling%3Dscale-down%26starting-point-node-id%3D50%253A270%26show-proto-sidebar%3D1%26mode%3Ddesign%26hide-ui%3D1" allowfullscreen></iframe>
