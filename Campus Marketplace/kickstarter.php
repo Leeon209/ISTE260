ebf661659fa9c233c25ab27f3c8a7c5dca23fe9d
Leeon <leeonsethanoun@gmail.com>
2023-12-18 16:06:55 -0800

    added kickstarter youtube video

---
+++ Campus Marketplace/kickstarter.php
@@ -0,0 +1,11 @@
+<?php
+$page = 'kickstarter';
+$path = '';
+include ($path.'asset/inc/header.php');
+include ($path.'assets/inc/nav.php');
+?>
+<div id ="kickstarter">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/WTaht7_AvPs?si=Yj8kJK0AsWqpBi20" title="BigHero6 Team6 Kickstarter" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+</body>
+</html>
